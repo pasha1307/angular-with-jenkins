cb3a45f60d37d3a8716b2eb0c492903267553542
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2021-12-15 16:11:26 -0500

    feature module is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To Integrate With Jenkins';
+  title = 'Updated Title To Integrate With Jenkins';
 }
